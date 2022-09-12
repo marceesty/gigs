fece471600dee867394f4ce03a22ca84fa0f2028
marceesty <marceesty@gmail.com>
2022-09-12 12:44:40 +0100

    slice added

--- slice.go
+++ slice.go
@@ -5,7 +5,6 @@ import "fmt"
 
 func main() {
 	words := make([] string, 13)
-	numbers := []int{2,3,4,5}
 	words[0] = "on"
 	words[1] = "go"
 	words[2] = "make"
@@ -15,5 +14,4 @@ func main() {
 	words[6] = "jump"
 	words[12] = "eye"
 	fmt.Println(words)
-	fmt.Println(numbers)
 }
